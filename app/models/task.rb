0b1de3836432b4dcfe56c41774d92f19d7e9fb38
Malakhova Natasha <8430654@gmail.com>
2019-11-22 22:50:50 +0300

    Update task.rb

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,78 +1,35 @@
 class Task < ApplicationRecord
 
-  def self.allTasks
-    tasks_all = Task.all
-    tasks_all .each do |task|
-      puts "
-            id: #{task.id}
-            tasks: #{task.tasks}
-            done: #{task.done}
-            data_create: #{task.created_at}
-            data_update: #{task.updated_at}"
-    end
-  end
-
-  def self.unfinishedTasks
-    tasks_uncomplited = Task.where(done: 0).order(:created_at)
-    tasks_uncomplited .each do |task|
-      puts "
-            id: #{task.id}
-            tasks: #{task.tasks}
-            done: #{task.done}
-            data_create: #{task.created_at}
-            data_update: #{task.updated_at}"
-    end
-  end
-
-  def self.finishedTasks
-    tasks_complited = Task.where(done: 1).order(:created_at)
-    tasks_complited .each do |task|
-      puts "
-            id: #{task.id}
-            tasks: #{task.tasks}
-            done: #{task.done}
-            data_create: #{task.created_at}
-            data_update: #{task.updated_at}"
-    end
-  end
+  scope :unfinished, -> { where(done: false) }
+  scope :finished, -> { where(done: true) }
+  default_scope { order(created_at: :asc) }
 
   def self.add(text)
-    Task.create(:tasks => text, :done => false)
+    Task.create(text: text, done: false)
   end
 
-  def self.updateTask(id, newText)
-    Task.update(id, :tasks => newText)
+  def self.edit(id, new_text)
+    Task.update(id, text: new_text)
   end
 
-  def self.check(id)
-    taskChecked = Task.find(id)
-    check = !taskChecked.done
-    Task.update(id,:done => check)
+  def self.finish(id)
+    Task.update(id, done: true)
   end
 
-  def self.deleteTask(id)
+  def self.unfinish(id)
+    Task.update(id, done: false)
+  end
+
+  def self.del(id)
     Task.delete(id)
   end
 
-  def self.deleteFinished
-    tasksFinishedDel = Task.where(done: 1)
-    tasksFinishedDel .each do |task|
-      Task.delete(task.id)
-    end
+  def self.delete_finished
+    Task.finished.delete_all
   end
 
-  def self.deleteUnfinished
-    tasksUnfinishedDel = Task.where(done: 0)
-    tasksUnfinishedDel .each do |task|
-      Task.delete(task.id)
-    end
-  end
-
-  def self.deleteAll
-    tasksDel = Task.all
-    tasksDel .each do |task|
-      Task.delete(task.id)
-    end
+  def self.delete_unfinished
+    Task.unfinished.delete_all
   end
 
 end
