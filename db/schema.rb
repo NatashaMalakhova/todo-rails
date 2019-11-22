a51fa57aa0abbd363933873c1290c02aacab85c1
Malakhova Natasha <8430654@gmail.com>
2019-11-22 22:50:29 +0300

    rename column

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_16_151430) do
+ActiveRecord::Schema.define(version: 2019_11_22_114218) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string "tasks"
+    t.string "text"
     t.boolean "done"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
